d5c6340578b71b531fd9cc184a4734181a423fa1
Sourav Bhukta <souravbhukta8@gmail.com>
2024-08-15 12:08:13 +0530

    mis

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.use(express.json())
 app.use(cookieParser())
 
 app.use(cors({
-    origin: 'https://sixvalley-frontend.vercel.app/',
+    origin: 'https://sixvalley-frontend.vercel.app',
     credentials: true
 }))
 
